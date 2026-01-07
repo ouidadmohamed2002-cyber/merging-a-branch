efdd46fd468140798d8152d15a62b1462944a282
Sanae-123-wq <sanaeelbelghiti2@gmail.com>
2026-01-07 11:31:54 +0100

    sanae

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,17 @@
-export default function hello(){
+ function hello(){
   return (
     <>
     <h1>hello ouidad</h1>
     </>
   )
 }
+
+
+ function hi(){
+  return (
+    <>
+    <h1>hello sanae</h1>
+    </>
+  )
+}
+export default hello
